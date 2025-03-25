9ed67fe8493b6f0db43b48e32cbc16a31f7b9b6b
bashar-oudeb <bacharouutaib@gmail.com>
2025-03-25 12:26:12 +0000

    Enhance Loader component with loading state and smooth transition effects for improved user experience

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import profile from "../../assets/profile1.png";
 
 const Loader = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
     <div className="relative w-56 h-56 flex items-center justify-center">
       {/* Spinning border ring */}
       <div
-        className=" absolute inset-0 rounded-full animate-pulse
- bg-gradient-to-tr from-primary via-tertiary to-secondary  blur-sm shadow"
+        className="absolute inset-0 rounded-full animate-pulse
+        bg-gradient-to-tr from-primary via-tertiary to-secondary blur-sm shadow"
       >
         {/* Inner solid circle to create border effect */}
         <div className="w-full h-full bg-[#242424] rounded-full" />
@@ -16,10 +19,14 @@ const Loader = () => {
       <img
         src={profile}
         alt="Loader"
-        className="w-full h-full object-cover rounded-full z-10"
+        className="w-full h-full object-cover rounded-full z-10 transition-opacity duration-500"
         loading="lazy"
-        style={{ filter: "blur(10px)", transition: "filter 0.5s ease-in-out" }}
-        onLoad={(e) => (e.target.style.filter = "blur(0)")} 
+        style={{
+          filter: isLoaded ? "blur(0)" : "blur(10px)",
+          transition: "filter 0.5s ease-in-out, opacity 0.5s ease-in-out",
+          opacity: isLoaded ? 1 : 0,
+        }}
+        onLoad={() => setIsLoaded(true)}
       />
     </div>
   );
